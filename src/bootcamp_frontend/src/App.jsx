58e5ab13bcd88aa760c42a6b29639ae2d6704c3e
Woolioo <michucity17@gmail.com>
2024-06-25 15:53:23 +0200

    added lorem ipsum text

--- src/bootcamp_frontend/src/App.jsx
+++ src/bootcamp_frontend/src/App.jsx
@@ -3,6 +3,8 @@ function App() {
   return (
     <div className="mainApp">
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti magni minus veniam, rem officiis distinctio harum id vero eligendi repudiandae! Similique harum nostrum eligendi, aperiam quod itaque ipsa ex autem omnis consequuntur. Nulla architecto sit dolore atque quisquam blanditiis, repellat voluptatum similique possimus inventore suscipit id. Aliquid magnam nobis, tenetur facere culpa rerum deleniti eius libero iure corrupti laboriosam adipisci sed sequi vero rem, unde in. Deleniti iusto ex soluta maxime dolorem omnis quisquam amet fugit ea expedita est, quaerat minus maiores excepturi commodi, totam neque eaque optio modi esse facilis consequatur quidem magni? Sint nam tenetur velit nostrum aut?</p> 
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus adipisci dolorem, aperiam repellat voluptatibus dolores at nesciunt voluptatum quam in tenetur laborum ea eveniet quidem aliquid vitae optio provident commodi!</p>
     </div>
   );  
 }
